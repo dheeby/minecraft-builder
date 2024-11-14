47e6260111a1687a568a07636da2dbef4679b3f6
Neil Allison <neiljallison@gmail.com>
2024-11-14 11:32:01 -0500

    Unused

--- app/builder/Controls.tsx
+++ app/builder/Controls.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@mui/material";
 
 export default function Controls() {
+  const onAddBlockClick = () => {
+    alert(
+      "TODO: Implement adding a block, probably a search+drag+drop type of thing instead of a button"
+    );
+  };
+
   return (
     <div>
-      <Button>Add Block</Button>
+      <Button onClick={onAddBlockClick}>Add Block</Button>
     </div>
   );
 }
